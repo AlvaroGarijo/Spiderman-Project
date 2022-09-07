d201281695e3eae7c728226c2065776da1ea5e8e
AlvaroGarijo <agarijo@padelmanager.com>
2022-09-07 16:23:58 +0200

    primera imagen

--- src/componentes/CenterApp.js
+++ src/componentes/CenterApp.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import '../styles/CenterApp.css'
+import React from 'react';
+import '../styles/CenterApp.css';
+import  agenteVenom  from '../imagenes/agenteVenom.jpeg'
+
+
 
 export const CenterApp = () => {
   return (
-    <div>CenterApp</div>
+    <div>
+      <h1>Soy una imagen</h1>
+      <img  className='image1' 
+            src={agenteVenom} 
+            alt='Agente-Venom'/>
+    </div>
   )
 }
