9ab9f2e32c2f51543a398242da2f12542a36ed1d
AlvaroGarijo <agarijo@padelmanager.com>
2022-09-07 02:34:35 +0200

    Borrado project añadido centerApp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
+import { CenterApp } from './componentes/CenterApp';
+import { Footer } from './componentes/Footer';
+import { Navbar } from './componentes/Navbar';
 
 function App() {
   return (
     <div className="App">
      <h1>Spider-Man</h1>
+      <Navbar/>
+      <CenterApp/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/componentes/CenterApp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CenterApp = () => {
+  return (
+    <div>CenterApp</div>
+  )
+}
