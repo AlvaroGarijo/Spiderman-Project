86198f6afbd7388b14de7c85506fac4b8e3c022a
AlvaroGarijo <agarijo@padelmanager.com>
2022-09-11 23:58:45 +0200

    Fuente Fira-Code eliminada, uso de monospace

--- src/componentes/Navegacion/InicioWeb.js
+++ src/componentes/Navegacion/InicioWeb.js
@@ -24,7 +24,7 @@ export const InicioWeb = () => {
         </div> 
         <div className="noOficialWrapp">
             <img className="logoSpider" src={ logoSpider }/>
-            <p className="no-oficial">Página web NO oficial de Spider-Man hecha con cariño hacia el personaje.</p>
+            <p className="no-oficial">Página web NO oficial de Spider-Man hecha con cariño hacia el personaje. Fuentes: Wikipedia.</p>
         </div>
     </div>
   )
